611f6b6da93db3c4af99c8587fb09c844c66731e
Layer8 <NWillAU900@gmail.com>
2023-04-12 18:53:35 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,77 +1,34 @@
 #!/bin/bash
 
-#Tell the installer the root of the files to download
-REPO="https://raw.githubusercontent.com/nicknacnic/IB-L8/master/"
+# Remove the directory for the primary IB-L8 code to live
+rm -r /var/ibl8
 
-#check to make sure Python is installed
-python --version
+# Remove the devices database
+rm create.sql
+rm devices.sql 
+# Install the code that updates the firewall
+sudo wget -q ${REPO}ibl8.py
 
-#install the python requests module
-sudo apt-get install python-requests -y
+# Create the log file
+sudo touch ibl8.log
 
-#install sqlite3
-sudo apt-get install sqlite3 -y
-
-#install python-pip
-sudo apt-get install python-pip -y
-
-#install xmldiff
-sudo pip install xmldiff
-
-#create the directory for the primary dug code to live
-cd /var
-sudo mkdir dug
-sudo chown www-data dug
-sudo chgrp www-data dug
-cd /var/dug
-#create the devices database
-sudo touch create.sql
-sudo chmod 777 create.sql
-sudo echo 'CREATE TABLE DevicesDynamic (DeviceName "TEXT", DeviceMac "TEXT", Groups "Text");' > create.sql
-sudo sqlite3 devices.sql < create.sql
-sudo rm create.sql
-#install the code that updates the firewall
-sudo wget -q ${REPO}dug.py
-#create the log file
-sudo touch dug.log
-#Set owner, group, and permissions of files in /var/dug
+# Set owner, group, and permissions of files in /var/ibl8
 sudo chown www-data *.*
 sudo chgrp www-data *.*
 sudo chmod 755 *.*
 
 #update cron to execute the script every minute
 cd /etc/cron.d
-sudo wget -q ${REPO}dugcron
+sudo wget -q ${REPO}ibl8cron
 
 #install apache2 and configure it to allow cgi
-sudo apt-get install apache2 -y
+sudo apt install apache2 -y
 sudo a2enmod cgid
 sudo service apache2 restart
 
-#copy cgi scripts into the cgi directory
-cd /usr/lib/cgi-bin
-sudo wget -q ${REPO}index.cgi
-sudo wget -q ${REPO}keygen.cgi
-sudo wget -q ${REPO}vlan.cgi
-sudo wget -q ${REPO}usermap.cgi
-sudo wget -q ${REPO}groupmap.cgi
-sudo wget -q ${REPO}clearusers.cgi
-sudo wget -q ${REPO}arp.cgi
-sudo wget -q ${REPO}dhcp.cgi
-sudo wget -q ${REPO}dhcputil.cgi
-sudo wget -q ${REPO}policy.cgi
-sudo wget -q ${REPO}duglog.cgi
-sudo wget -q ${REPO}syslog.cgi
-sudo wget -q ${REPO}messageslog.cgi
-sudo wget -q ${REPO}accesslog.cgi
-sudo wget -q ${REPO}errorlog.cgi
-sudo wget -q ${REPO}manback.cgi
-sudo wget -q ${REPO}software.cgi
-sudo wget -q ${REPO}changes.cgi
-sudo wget -q ${REPO}menu.html
-sudo chown www-data *.*
-sudo chgrp www-data *.*
-sudo chmod 755 *.*
+# Remove cgi scripts into the cgi directory
+rm -r /usr/lib/cgi-bin
+
 
 #log permissions and rotation configuration
 sudo chmod 644 /var/log/syslog
