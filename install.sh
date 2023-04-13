dd2cb4c9a89424aec65630dbbdc2f97e6f231136
Layer8 <NWillAU900@gmail.com>
2023-04-12 18:54:00 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,33 @@
 #!/bin/bash
+# Tell the installer the root of the files to download
+REPO="https://raw.githubusercontent.com/nicknacnic/IB-L8/master/"
+:<<'COMMENT'
 
-# Remove the directory for the primary IB-L8 code to live
-rm -r /var/ibl8
+# Install the latest version of Python if it's not already installed
+if ! command -v python3 &>/dev/null; then
+    sudo apt update && sudo apt install -y python3
+fi
 
-# Remove the devices database
-rm create.sql
-rm devices.sql 
+# Install other dependencies
+sudo apt install -y sqlite3 python3-pip
+pip3 install xmldiff
+pip3 install requests
+
+# Create the directory for the primary IB-L8 code to live
+cd /var
+sudo mkdir ibl8
+sudo chown www-data ibl8
+sudo chgrp www-data ibl8
+cd /var/ibl8
+
+# Create the devices database
+sudo touch create.sql
+sudo chmod 777 create.sql
+sudo echo 'CREATE TABLE DevicesDynamic (DeviceName "TEXT", DeviceMac "TEXT", Groups "Text");' > create.sql
+sudo sqlite3 devices.sql < create.sql
+sudo rm create.sql
+COMMENT
+
 # Install the code that updates the firewall
 sudo wget -q ${REPO}ibl8.py
 
@@ -13,9 +35,9 @@ sudo wget -q ${REPO}ibl8.py
 sudo touch ibl8.log
 
 # Set owner, group, and permissions of files in /var/ibl8
-sudo chown www-data *.*
-sudo chgrp www-data *.*
-sudo chmod 755 *.*
+sudo find /var/ibl8 -exec chown www-data {} \;
+sudo find /var/ibl8 -exec chgrp www-data {} \;
+sudo find /var/ibl8 -type f -exec chmod 755 {} \;
 
 #update cron to execute the script every minute
 cd /etc/cron.d
@@ -26,9 +48,30 @@ sudo apt install apache2 -y
 sudo a2enmod cgid
 sudo service apache2 restart
 
-# Remove cgi scripts into the cgi directory
-rm -r /usr/lib/cgi-bin
-
+#copy cgi scripts into the cgi directory
+cd /usr/lib/cgi-bin
+sudo wget -q ${REPO}index.cgi
+sudo wget -q ${REPO}keygen.cgi
+sudo wget -q ${REPO}vlan.cgi
+sudo wget -q ${REPO}usermap.cgi
+sudo wget -q ${REPO}groupmap.cgi
+sudo wget -q ${REPO}clearusers.cgi
+sudo wget -q ${REPO}arp.cgi
+sudo wget -q ${REPO}dhcp.cgi
+sudo wget -q ${REPO}dhcputil.cgi
+sudo wget -q ${REPO}policy.cgi
+sudo wget -q ${REPO}ibl8log.cgi
+sudo wget -q ${REPO}syslog.cgi
+sudo wget -q ${REPO}messageslog.cgi
+sudo wget -q ${REPO}accesslog.cgi
+sudo wget -q ${REPO}errorlog.cgi
+sudo wget -q ${REPO}manback.cgi
+sudo wget -q ${REPO}software.cgi
+sudo wget -q ${REPO}changes.cgi
+sudo wget -q ${REPO}menu.html
+sudo find /usr/lib/cgi-bin -exec chown www-data {} \;
+sudo find /usr/lib/cgi-bin -exec chgrp www-data {} \;
+sudo find /usr/lib/cgi-bin -type f -exec chmod 755 {} \;
 
 #log permissions and rotation configuration
 sudo chmod 644 /var/log/syslog
@@ -64,9 +107,9 @@ sudo wget -q ${REPO}style.css
 sudo wget -q ${REPO}favicon.ico
 sudo touch macs.txt
 sudo touch rsa.csv
-sudo chown www-data *.*
-sudo chgrp www-data *.*
-sudo chmod 755 *.*
+sudo find /var/www/html -exec chown www-data {} \;
+sudo find /var/www/html -exec chgrp www-data {} \;
+sudo find /var/www/html -type f -exec chmod 755 {} \;
 
 cd /var
 sudo mkdir autoback
@@ -74,9 +117,9 @@ sudo chown www-data autoback
 sudo chgrp www-data autoback
 cd /var/autoback
 sudo wget -q ${REPO}autoback.py
-sudo chown www-data *.*
-sudo chgrp www-data *.*
-sudo chmod 755 *.*
+sudo find /var/autoback -exec chown www-data {} \;
+sudo find /var/autoback -exec chgrp www-data {} \;
+sudo find /var/autoback -type f -exec chmod 755 {} \;
 
 #harden the Raspberry Pi
 sudo systemctl disable avahi-daemon
